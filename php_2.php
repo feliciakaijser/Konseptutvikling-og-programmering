f49a51532ff7342b8d11c600c4612a3259a4bbca
feliciakaijser <71969626+feliciakaijser@users.noreply.github.com>
2020-10-05 22:30:26 +0200

    Create php_2.php

---
+++ php_2.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+    <title> Kapittel 10 </title>
+    <!-- UTF-8 gjør at vi kan bruke æ, ø og å -->
+    <meta charset="UTF-8">
+ 
+</head>
+<body>
+    <h1> Felicias php-dokument </h1>
+
+<?php
+// her angis variablene $tall. Den får verdien 23.
+$tall = 23;
+echo "<p>Tallet er $tall. </p>"; 
+
+//Her blir verdien i variabelen $tall endret til 42.
+$tall = 42;
+echo "<p> Nå er tallet $tall. </p>;
+?>
+<p> Fordi denne teksten ikke er omsluttet av php-taggene, kan jeg skrive som om jeg jobber i et vanligt HTML-dokument, </p>
+<?php
+echo "<p> Her er vi tilbake i PHP, da må jeg bruke echo for å skrive HTML-kode. </p>";
+echo "<p> Variabelen jeg definerte i starten av dokumentet, finnes fremledes, tallet er $tall. </p>";
+?>
+
+</body>
+</html>
